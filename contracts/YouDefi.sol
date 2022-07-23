565a3e4a6de39e074edac69dcff4ab021aff2574
Arjun Dev <arjun@Arjuns-MacBook-Pro.local>
2022-07-24 02:18:36 +0530

    feat: added loan component to smartcontract

--- contracts/YouDefi.sol
+++ contracts/YouDefi.sol
@@ -5,13 +5,20 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract YouDefi {
     struct Terms {
+        address tokenAddress;
+        address lender;
+        address borrower;
         uint256 loanDaiAmount;
         uint feeDaiAmount;
         uint ethCollateralAmount;
         uint repayByTimestamp;
+        string status;
     }
 
     Terms public terms;
+
+    uint public numOfLoans = 0;       // Total number of loans in the smart contract
+    mapping (uint => Terms) public Loans; // Loan ID
 
     enum LoanState {Created, Funded, Taken}
     LoanState public state;
@@ -25,7 +32,7 @@ contract YouDefi {
         // daiAddress = _daiAddress;
         // lender = payable(msg.sender);
         // state = LoanState.Created;
-        token = IERC20(0xd9145CCE52D386f254917e481eB44e9943F39138); 
+        // token = IERC20(0xd9145CCE52D386f254917e481eB44e9943F39138); 
     }
 
     modifier onlyInState(LoanState expectedState) {
@@ -63,5 +70,42 @@ contract YouDefi {
 
     function getTimestamp() public view returns(uint){
         return block.timestamp;
+    }
+
+    function NewinitiateLoan(address _tokenAddress, uint _loanAmt, uint _fee, uint _collateral, uint _timestamp) public {
+        numOfLoans++;
+        Loans[numOfLoans].lender = payable(msg.sender);
+        Loans[numOfLoans].loanDaiAmount = _loanAmt;
+        Loans[numOfLoans].feeDaiAmount = _fee;
+        Loans[numOfLoans].ethCollateralAmount = _collateral;
+        Loans[numOfLoans].repayByTimestamp = _timestamp;
+        Loans[numOfLoans].tokenAddress =  _tokenAddress;
+        Loans[numOfLoans].status = "Loan Created";
+        IERC20(_tokenAddress).transferFrom(
+            msg.sender,
+            address(this),
+            _loanAmt
+        );
+    }
+
+    function NewtakeLoan(uint _LoanID) public payable {
+        require(msg.value == Loans[_LoanID].ethCollateralAmount, "Invalid Collateral Amount");
+        Loans[_LoanID].borrower = msg.sender;
+        IERC20(Loans[_LoanID].tokenAddress).transfer(Loans[_LoanID].borrower, Loans[_LoanID].loanDaiAmount);
+        Loans[_LoanID].status = "Loan Borrowed";
+    }
+
+    function NewRepay(uint _LoanID) public {
+        require(msg.sender == Loans[_LoanID].borrower, "Only the borrower can repay the loan");
+        IERC20(Loans[_LoanID].tokenAddress).transferFrom(Loans[_LoanID].borrower, Loans[_LoanID].lender, Loans[_LoanID].loanDaiAmount + Loans[_LoanID].feeDaiAmount);
+        payable(msg.sender).transfer(Loans[_LoanID].ethCollateralAmount);
+        Loans[_LoanID].status = "Loan Repaid";
+    }
+
+    function Newliquidate(uint _LoanID) public {
+        require(msg.sender == Loans[_LoanID].lender, "Only the lender can liquidate the loan");
+        require(block.timestamp >= Loans[_LoanID].repayByTimestamp, "Can not liquidate before the loan is due");
+        payable(msg.sender).transfer(Loans[_LoanID].ethCollateralAmount);
+        Loans[_LoanID].status = "Loan Defaulted";
     }
 }
